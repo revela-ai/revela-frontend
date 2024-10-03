3497f6be85657ea4a2fdf95ae10a13a8767527c0
Bernard <bernardsackey95@gmail.com>
2024-10-03 10:46:48 +0000

    minor ui fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -116,7 +116,7 @@ export default function NavigationBar() {
               </div>
             </div>
             <div className="min-h-[87vh] md:min-h-0 flex lg:w-full">
-              <div className="text-center lg:ms-auto flex-col md:flex-row flex items-center justify-center gap-4 md:pt-0 pt-5 mt-auto w-full lg:w-fit">
+              <div className="text-center lg:ms-auto flex-col md:flex-row flex items-center justify-center gap-4 md:pt-0 pt-5 mx-auto w-full lg:w-fit">
                 <AlertDialog>
                   <AlertDialogTrigger asChild>
                     <Button className="rounded-full w-full h-12 lg:h-8 bg-transparent border border-primary text-primary hover:text-white">
